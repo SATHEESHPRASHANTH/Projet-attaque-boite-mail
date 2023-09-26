6dd7336bb2dacb794d5c11cdd6c97d6958eb92e4
Marc-Andy67 <144818191+Marc-Andy67@users.noreply.github.com>
2023-09-26 16:52:42 +0200

    Add files via upload

--- Projet/pageDeContact.php
+++ Projet/pageDeContact.php
@@ -1,4 +1,20 @@
+<?php
+        $servername = 'localhost';
+            $username = 'root';
+            $password = 'root';
+            $dbname = "tableprojet";
+            
+            //On établit la connexion
+            $conn = new mysqli($servername, $username, $password);
+            
+            //On vérifie la connexion
+            if($conn->connect_error){
+                die('Erreur : ' .$conn->connect_error);
+            }
+            $sql = "SELECT Idcontact, Nom, Prenom ,Mail , Tel , Adresse , CP , Ville , Societe FROM contact";
+?>
 <html>
+    <form action="cnx.php" method="post">
     <link rel="stylesheet" href="style3.css">
     <div class="flex wrap">
         <div class="main">
@@ -13,12 +29,9 @@
         </div>
         <div class="optional">
             <ul>Liste de contact<hr>
-                <li>
-                    Nom/Prénom/Mail/Tel/Adresse/CodePostal/Ville/societe<br><hr> 
-                </li>
-                <li>
-                    Nom/Prénom/Mail/Tel/Adresse/CodePostal/Ville/societe<br><hr> 
-                </li>
+            <?php
+            echo $_POST[1]."<br>";
+            ?>
             </ul></div>
    </div>
 </html>
