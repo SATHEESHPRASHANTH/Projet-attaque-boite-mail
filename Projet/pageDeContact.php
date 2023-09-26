3739d3576e3221d2dbb087905d457278e3f4a05b
Marc-Andy67 <144818191+Marc-Andy67@users.noreply.github.com>
2023-09-26 15:01:39 +0200

    Add files via upload

---
+++ Projet/pageDeContact.php
@@ -0,0 +1,24 @@
+<html>
+    <link rel="stylesheet" href="style3.css">
+    <div class="flex wrap">
+        <div class="main">
+            <ul>Menu<hr>
+                <li>
+                    <a>boite principale</a><hr>
+                </li>
+                <li>
+                    page de contact<hr>
+                </li>
+            </ul>
+        </div>
+        <div class="optional">
+            <ul>Liste de contact<hr>
+                <li>
+                    Nom/Prénom/Mail/Tel/Adresse/CodePostal/Ville/societe<br><hr> 
+                </li>
+                <li>
+                    Nom/Prénom/Mail/Tel/Adresse/CodePostal/Ville/societe<br><hr> 
+                </li>
+            </ul></div>
+   </div>
+</html>
